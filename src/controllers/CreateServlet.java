d0feb8c81059985f4e1c138983bfe37c0d7bf207
daisuke.mine <tensaidaisuke03@gmail.com>
2022-07-02 23:21:21 +0900

    ShowServlet.javaを作成

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -33,7 +33,7 @@ public class CreateServlet extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String _token = request.getParameter("_token");
-        if(_token !=null && _token.equals(request.getSession().getId())) {
+        if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
             em.getTransaction().begin();
 
